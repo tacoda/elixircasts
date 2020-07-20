488a18804d793dd08eccbd5a8cfdd623345e4269
Ian Johnson <tacoda@pm.me>
2020-07-20 11:33:08 -0400

    Add pattern matching exercise

---
+++ 05_pattern_matching/matcher.exs
@@ -0,0 +1,11 @@
+defmodule Matcher do
+
+  def get_msg(response) do
+    case response do
+      {:ok, msg} ->
+        "Success: #{msg}"
+      {:error, msg} ->
+        "Error: #{msg}"
+    end
+  end
+end
